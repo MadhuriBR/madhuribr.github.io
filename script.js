c689e2a3d674bc72ff5c76a3136a0b0ec138de8b
anthony tran <atran@users.noreply.github.com>
2024-08-06 17:42:07 -0700

    adjust timing on entrance

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loaderAnimation() {
     var loader = document.querySelector("#loader")
     setTimeout(function () {
         loader.style.top = "-100%"
-    }, 4200)
+    }, 2000)
 }
 
 swiperAnimation()
